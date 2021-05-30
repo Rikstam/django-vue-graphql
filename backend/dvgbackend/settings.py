6659227466a7f000bdbe59da6d0644deaa252cfd
Riku <rikukestila@gmail.com>
2021-05-30 18:16:14 +0300

    Add graphQL

--- backend/dvgbackend/settings.py
+++ backend/dvgbackend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'graphene_django'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE = {
+    "SCHEMA": "blog.schema.schema",
+}
